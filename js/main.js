2781558a1fd61171df56aea553d544df85fc18d3
Infared! <ekkirkitadze@gmail.com>
2023-08-25 00:06:26 +0300

    feat: create form container

--- js/main.js
+++ js/main.js
@@ -4,8 +4,11 @@ $('.slider__inner').slick({
     nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     infinite: false
+
+
 });
 
+$('select').styler();
 
 
 
